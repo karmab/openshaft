10685c7e5d3d7db65165fc1e3d544560604b9295
karimb <karimboumedhel@gmail.com>
2017-07-03 22:18:36 +0200

    improvements on the openshift plan

--- kcli/advanced/install2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-ansible-playbook -i /root/hosts /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml
-htpasswd -b /etc/origin/master/htpasswd karim karim
-htpasswd -b /etc/origin/master/htpasswd malika malika
