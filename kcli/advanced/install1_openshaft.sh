2ecf0310f03eb550fc39d1b275625c9e0b58a7ad
karimb <karimboumedhel@gmail.com>
2017-07-03 18:43:56 +0200

    advanced openshift inventory

--- kcli/advanced/install1_openshaft.sh
+++
@@ -1,5 +0,0 @@
-yum -y install openshift-ansible-playbooks
-ssh-keyscan -H master.karmalabs.local >> ~/.ssh/known_hosts
-ssh-keyscan -H node01.karmalabs.local >> ~/.ssh/known_hosts
-ssh-keyscan -H node02.karmalabs.local >> ~/.ssh/known_hosts
-ssh-keyscan -H node03.karmalabs.local >> ~/.ssh/known_hosts
