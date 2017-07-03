221cc4f4e523aaeafc5214af03aa597d7b4b844f
karimb <karimboumedhel@gmail.com>
2017-07-03 20:03:27 +0200

    correct location for install script in openshift plan

--- kcli/advanced/install2.sh
+++ kcli/advanced/install2.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 ansible-playbook -i /root/hosts /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml
-for i in 1 2 3 ; do 
-  ssh master${i}.karmalabs.local "htpasswd -b /etc/origin/master/htpasswd karim karim"
-done
+htpasswd -b /etc/origin/master/htpasswd karim karim
+htpasswd -b /etc/origin/master/htpasswd malika malika
