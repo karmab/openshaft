8ac92552c88d4824f3558f45024fc162da585d6d
karimb <karimboumedhel@gmail.com>
2017-07-07 23:26:51 +0200

    proper waits when deploying and kvm specific cpu features

--- kcli/advanced/compute.sh
+++ kcli/advanced/compute.sh
@@ -1,1 +1,4 @@
 echo options kvm_intel nested=1 > /etc/modprobe.d/kvm.conf
+echo net.ipv4.conf.all.rp_filter=0 >> /etc/sysctl.d/99-sysctl.conf
+echo net.ipv4.conf.default.rp_filter=0 >> /etc/sysctl.d/99-sysctl.conf
+sysctl -p /etc/sysctl.conf
