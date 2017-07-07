3ee156ce54b98fcc3b85454846de4101c7b081be
karimb <karimboumedhel@gmail.com>
2017-07-07 23:26:01 +0200

    proper waits when deploying and kvm specific cpu features

--- openshift/create.sh
+++ openshift/create.sh
@@ -7,8 +7,10 @@ oc create -f pvglance.yml
 
 oc create -f mysql.yml
 oc create -f rabbit.yml
+sleep 120
 
 oc create -f keystone.yml
+sleep 120
 
 oc create -f glance.yml
 oc create -f cinder.yml
@@ -16,6 +18,7 @@ oc create -f neutron.yml
 oc create -f nova.yml
 oc create -f heat.yml
 oc create -f horizon.yml
+sleep 120
 
 oc create -f cinder-volume.yml
 oc create -f neutron-agents.yml
